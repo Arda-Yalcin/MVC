a554f988f9f63dff97da5e3fa66a5fe633d50d84
Arda Yalçın <yalcinarda734@gmail.com>
2026-02-03 10:47:10 +0300

    mvc 2. sayfa

---
+++ MvcOrnek1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MvcOrnek1.Models;
+
+namespace MvcOrnek1.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Deneme()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
